bdcd530a645dbf8b29ea037507b51b3c7560eb66
BinyominC <binyominc@ltccs.com>
2020-03-31 15:19:56 -0400

    validates jwt

--- handler.py
+++ handler.py
@@ -1,6 +1,10 @@
 import boto3
 import json
 import os
+import requests
+from jose import jwt, jwk
+from jose.utils import base64url_decode
+
 """
 Request body should be like the following
 {
@@ -31,6 +35,16 @@ def handler(event, context):
                 "headers": {"Content-Type": "application/json"},
                 "body": json.dumps({"error": "hmm we dont let this"})
             }
+
+        jwks = get_jwks()
+        id_token = event_body['id_token']
+        if not verify_jwt(id_token, jwks):
+            return {
+                "statusCode": 403,
+                "headers": {"Content-Type": "application/json"},
+                "body": json.dumps({"error": "invalid id token"})
+            }
+
 
         if action == GET_DETAILS:
             ...
@@ -86,3 +100,30 @@ def get_details(event_body):
     )
 
     return medicaid_details
+
+
+def get_jwks():
+    return requests.get(
+        f"https://cognito-idp.us-east-1.amazonaws.com/us-east-1_IiYvInxsJ/.well-known/jwks.json"
+    ).json()
+
+
+def get_hmac_key(token: str, jwks):
+    kid = jwt.get_unverified_header(token).get("kid")
+    for key in jwks.get("keys", []):
+        if key.get("kid") == kid:
+            return key
+
+
+def verify_jwt(token: str, jwks) -> bool:
+    hmac_key = get_hmac_key(token, jwks)
+
+    if not hmac_key:
+        raise ValueError("No pubic key found!")
+
+    hmac_key = jwk.construct(get_hmac_key(token, jwks))
+
+    message, encoded_signature = token.rsplit(".", 1)
+    decoded_signature = base64url_decode(encoded_signature.encode())
+
+    return hmac_key.verify(message.encode(), decoded_signature)
