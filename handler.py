5e3b83ac26fb440664b1498bd4c2500bafeb17e3
Binyomin Cohen <binyominco@gmail.com>
2020-04-29 15:59:17 -0400

    handles uuid and extra meta for each value

--- handler.py
+++ handler.py
@@ -6,6 +6,8 @@ import os
 import requests
 from jose import jwt, jwk
 from jose.utils import base64url_decode
+import datetime
+import uuid
 s3 = boto3.resource('s3')
 bucket_name = os.environ.get('USER_FILES_BUCKET', 'turbocaid--user-files--sps-qa-1')
 
@@ -77,6 +79,14 @@ responses = {
 }
 
 
+class MedicaidDetail:
+    def __init__(self, uuid, created_date, updated_date, value):
+        self.uuid = uuid
+        self.created_date = created_date
+        self.updated_date = updated_date
+        self.value = value
+
+
 def handler(event, context):
     try:
         print(str(event))
@@ -137,10 +147,76 @@ def handler(event, context):
     }
 
 
+def is_list_type(key_to_update):
+    array_types = [
+        'contacts', 'previous_addresses'
+    ]
+
+    return key_to_update in array_types
+
+
+def convert_to_medicaid_details_list(key_to_update, value_to_update, val_from_db):
+    dict_of_db_vals = {item['uuid']: item for item in val_from_db}
+
+    # dict_by_uuid = {}  # {'u8982-w98rw9r': {theitemfromdb}}
+    # for medicaid_detail in db_list:
+    #     dict_by_uuid[medicaid_detail['uuid']] = medicaid_detail
+
+    medicaid_details_to_insert = []
+    for detail_item_to_update in value_to_update:
+        now = datetime.datetime.now().isoformat()
+
+        medicaid_detail = MedicaidDetail(updated_date=now, value=detail_item_to_update['value'])
+
+        if 'uuid' in detail_item_to_update:
+            the_uuid = detail_item_to_update['uuid']
+            try:
+                db_item = dict_of_db_vals[uuid]
+            except Exception as err:
+                print(f'could not find corresponding item in db with uuid {the_uuid}')
+            medicaid_detail.uuid = the_uuid
+            medicaid_detail.created_date = db_item['created_date']
+        else:
+            medicaid_detail.uuid = uuid.uuid4()
+            medicaid_detail.created_date = datetime.datetime.now().isoformat()
+        
+        medicaid_details_to_insert.append(medicaid_detail)
+        
+        return medicaid_details_to_insert
+
+
+def convert_to_medicaid_detail(key_to_update, value_to_update, val_from_db):
+    now = datetime.datetime.now().isoformat()
+    medicaid_detail = MedicaidDetail(updated_date=now, value=value_to_update['value'])
+    if val_from_db:     
+        medicaid_detail.uuid = val_from_db['uuid']
+        medicaid_detail.created_date = val_from_db['created_date']
+    else:
+        medicaid_detail.uuid = uuid.uuid4()
+        medicaid_detail.created_date = now
+        
+    return medicaid_detail
+
+
+def get_db_value(email, key_to_update):
+    pass
+
+
 def update_details(email, event_body):
     print(str(event_body))
     key_to_update = event_body['key_to_update']
     value_to_update = event_body['value_to_update']
+
+    # if array -> map to proper medicaid details preserving or creating created date and uuid of what's already in db
+    # else -> update db while keeping or creating created date and uuid
+
+    val_from_db = get_db_value(email, key_to_update)
+    value_to_update_medicaid_detail_format = None
+    
+    if is_list_type(key_to_update):
+        value_to_update_medicaid_detail_format = convert_to_medicaid_details_list(key_to_update, value_to_update, val_from_db)
+    else:
+        value_to_update_medicaid_detail_format = convert_to_medicaid_detail(key_to_update, value_to_update, val_from_db)
 
     # The BatchWriteItem API allows us to write multiple items to a table in one request.
     resp = table.update_item(
@@ -150,7 +226,7 @@ def update_details(email, event_body):
         },
         # Expression attribute values specify placeholders for attribute values to use in your update expressions.
         ExpressionAttributeValues={
-            ":val_to_update": value_to_update,
+            ":val_to_update": value_to_update_medicaid_detail_format,
         },
         # UpdateExpression declares the updates we want to perform on our item.
         # For more details on update expressions, see https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.UpdateExpressions.html
