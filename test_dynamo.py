2a6afa68ce26aa7b1a2e270d0c924fee9880c716
Binyomin Cohen <binyominco@gmail.com>
2020-04-29 23:36:38 -0400

    makes tests robust and passing

--- test_dynamo.py
+++ test_dynamo.py
@@ -31,15 +31,44 @@ def clear_data():
 #     assert resp == "purple octopus"
 
 
-def test_update_details(clear_data):
+def test_update_details_non_list_value(clear_data):
+    VAL_TO_UPDATE = 'Shprintzah'
     event_body = {
         "action": "update-details",
         "key_to_update": "spouse_info_first_name",
-        "value_to_update": {'value':'Shprintzah'}
+        "value_to_update": {
+            "value": VAL_TO_UPDATE
+        }
     }
 
     update_details(EMAIL, event_body)
     resp = get_details(EMAIL)
 
     assert resp['Item']['email'] == EMAIL
-    assert resp['Item']['spouse_info_first_name'] == {'value':'Shprintzah'}
+    spouse_first_name_detail = resp['Item']['spouse_info_first_name']
+    assert spouse_first_name_detail['value'] == VAL_TO_UPDATE
+    length_of_normal_uuid = 32
+    assert len(spouse_first_name_detail['uuid']) == length_of_normal_uuid
+
+    SECOND_VAL_TO_UPDATE = 'Yentah'
+    second_event_body = {
+        "action": "update-details",
+        "key_to_update": "spouse_info_first_name",
+        "value_to_update": {
+            "value": SECOND_VAL_TO_UPDATE
+        }
+    }
+
+    update_details(EMAIL, second_event_body)
+    resp2 = get_details(EMAIL)
+
+    assert resp['Item']['email'] == EMAIL
+    spouse_first_name_detail2 = resp2['Item']['spouse_info_first_name']
+    assert spouse_first_name_detail2['value'] == SECOND_VAL_TO_UPDATE
+
+    # make sure the uuid and created date didn't change but updated date did
+    assert spouse_first_name_detail2['uuid'] == spouse_first_name_detail['uuid']
+    assert spouse_first_name_detail2['created_date'] == spouse_first_name_detail['created_date']
+    assert spouse_first_name_detail2['updated_date'] != spouse_first_name_detail['updated_date']
+
+
