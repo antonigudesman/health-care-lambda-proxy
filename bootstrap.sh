ec21ad4b85052cd05942e997c4256018876891ff
Binyomin Cohen <binyominco@gmail.com>
2020-05-07 18:54:38 -0400

    cleanup for use as subproject of aws lambda project manager

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo service docker start
 sudo docker pull amazon/dynamodb-local
 sudo docker run -d -p 8000:8000 amazon/dynamodb-local
 aws dynamodb create-table \
@@ -13,6 +10,3 @@ aws dynamodb create-table \
     --key-schema AttributeName=email,KeyType=HASH AttributeName=application_uuid,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
     --endpoint-url http://localhost:8000
-sudo yum install -y python3
-sudo python3 -m pip install pipenv
-cd /vagrant && export PIPENV_VENV_IN_PROJECT=1 && python3 -m pipenv install --skip-lock
