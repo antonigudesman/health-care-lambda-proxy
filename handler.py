3b9e2d825137540dbb89aa0fcf06fda8674aabeb
antonigudesman <antonigudesman@gmail.com>
2020-06-22 09:46:35 -0700

    refactor with utils

--- handler.py
+++ handler.py
@@ -1,8 +1,6 @@
-import os
 import base64
 import datetime
 
-import boto3
 import stripe
 
 from typing import Dict
@@ -13,6 +11,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from config import API_V1_STR, PROJECT_NAME
 from auth import get_email
+from utils import *
 from medicaid_detail_utils import *
 from response_helpers import (
     response_headers, missing_file_contents, missing_file_name,
@@ -30,76 +29,6 @@ app.add_middleware(
 )
 
 router = APIRouter()
-
-BUCKET_NAME = os.environ.get('USER_FILES_BUCKET')
-
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-table = dynamodb.Table(os.environ.get('TABLE', 'medicaid-details'))
-s3 = boto3.resource('s3')
-
-
-def update_dynamodb(email, application_uuid, key, val):
-    resp = table.update_item(
-        Key={'email': email, 'application_uuid': application_uuid},
-        ExpressionAttributeNames={ "#the_key": key },
-        ExpressionAttributeValues={ ":val_to_update": val },
-        UpdateExpression="SET #the_key = :val_to_update"
-    )
-
-    return resp
-
-
-def get_details(email, application_uuid):
-    medicaid_details = table.get_item(
-        Key={
-            'email': email, 'application_uuid': application_uuid
-        },
-        ConsistentRead=True,
-        ReturnConsumedCapacity='NONE',
-    )
-
-    return medicaid_details
-
-
-def get_db_value(email, key_to_update, application_uuid):
-    return get_details(email, application_uuid)['Item'].get(key_to_update, None)
-
-
-def is_list_type(key_to_update):
-    array_types = [
-        'contacts',
-        'previous_addresses',
-        'documents',
-        'general.vehicles',
-        'general-vehicles',
-        'employment_income.income_employment_details',
-        'employment_income-income_employment_details',
-        'insurance_policies.insurance_policies_details',
-        'insurance_policies-insurance_policies_details',
-        'general.properties',
-        'general-properties',
-        'general.property_proceeds',
-        'general-property_proceeds',
-        'financials.account_details',
-        'financials-account_details',
-        'financials.life_insurance_stocks_details',
-        'financials-life_insurance_stocks_details'
-    ]
-
-    return key_to_update in array_types
-
-
-def delete_document_info_from_database(user_email, event_body, application_uuid):
-    documents = get_db_value(user_email, 'documents', application_uuid)
-
-    file_name = event_body['file_name']
-    document_type = event_body['document_type']
-
-    clean_documents = [doc for doc in documents if doc['document_name'] != file_name or doc['document_type'] != document_type]
-
-    resp = update_dynamodb(user_email, application_uuid, 'documents', clean_documents)
-
-    return resp
 
 
 @router.post('/get-applications')
---
+++ utils.py
@@ -0,0 +1,126 @@
+import os
+
+import boto3
+
+
+BUCKET_NAME = os.environ.get('USER_FILES_BUCKET')
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table(os.environ.get('TABLE', 'medicaid-details'))
+s3 = boto3.resource('s3')
+
+
+def update_dynamodb(email, application_uuid, key, val):
+    resp = table.update_item(
+        Key={'email': email, 'application_uuid': application_uuid},
+        ExpressionAttributeNames={ "#the_key": key },
+        ExpressionAttributeValues={ ":val_to_update": val },
+        UpdateExpression="SET #the_key = :val_to_update"
+    )
+
+    return resp
+
+
+def get_details(email, application_uuid):
+    medicaid_details = table.get_item(
+        Key={
+            'email': email, 'application_uuid': application_uuid
+        },
+        ConsistentRead=True,
+        ReturnConsumedCapacity='NONE',
+    )
+
+    return medicaid_details
+
+
+def get_db_value(email, key_to_update, application_uuid):
+    return get_details(email, application_uuid)['Item'].get(key_to_update, None)
+
+
+def is_list_type(key_to_update):
+    array_types = [
+        'contacts',
+        'previous_addresses',
+        'documents',
+        'general.vehicles',
+        'general-vehicles',
+        'employment_income.income_employment_details',
+        'employment_income-income_employment_details',
+        'insurance_policies.insurance_policies_details',
+        'insurance_policies-insurance_policies_details',
+        'general.properties',
+        'general-properties',
+        'general.property_proceeds',
+        'general-property_proceeds',
+        'financials.account_details',
+        'financials-account_details',
+        'financials.life_insurance_stocks_details',
+        'financials-life_insurance_stocks_details'
+    ]
+
+    return key_to_update in array_types
+
+
+def delete_document_info_from_database(user_email, event_body, application_uuid):
+    documents = get_db_value(user_email, 'documents', application_uuid)
+
+    file_name = event_body['file_name']
+    document_type = event_body['document_type']
+
+    clean_documents = [doc for doc in documents if doc['document_name'] != file_name or doc['document_type'] != document_type]
+
+    resp = update_dynamodb(user_email, application_uuid, 'documents', clean_documents)
+
+    return resp
+
+
+def stripe_webhook(event_body, sig_header):
+    endpoint_secret = 'whsec_AtRlsWEBpMFNgNAjeqhxhQdDlRqFJOiE'
+    event= None
+    try:
+        event = stripe.Webhook.construct_event(
+            event_body, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        return {
+            "statusCode": 400,
+            "headers": response_headers
+        }
+
+    except stripe.error.SignatureVerificationError as e:
+        return {
+            "statusCode": 400,
+            "headers": response_headers
+        }
+
+    if event.type == 'checkout.session.succeeded':
+        checkout_session = event.data.object
+        handle_checkout_session_succeeded(checkout_session)
+    else:
+        return {
+            "statusCode": 400,
+            "headers": response_headers
+        }
+
+    return {
+        "statusCode": 200,
+        "headers": response_headers
+    }
+
+
+def handle_checkout_session_succeeded(checkout_session):
+    application_uuid = checkout_session.client_reference_id
+    payment_intent_id = checkout_session.payment_intent_id
+    payment_intent = stripe.PaymentIntent.retrieve(
+        payment_intent_id
+    )
+
+    payment_info_to_save = {
+        application_uuid: application_uuid,
+        amount_received: payment_intent.amount_received,
+        status: payment_intent.status,
+        customer_id: checkout_session.customer,
+        customer_email: checkout_session.customer_email
+    }
+
+    print(payment_info_to_save, 'payment info to save')
