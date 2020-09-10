ec3d763dc20756d5d4945ae0f38daaef2c892cc1
Ita Oppenheimer <itao@ltccs.com>
2020-09-10 11:33:15 -0400

    Combined successful payment and submit function

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 
+import datetime
 import boto3
 import stripe
 
@@ -58,7 +59,7 @@ def get_price_detail(email):
             ConsistentRead=True,
             ReturnConsumedCapacity='NONE',
             FilterExpression='standard = :standard',
-            ExpressionAttributeValues={':standard': 'true'}
+            ExpressionAttributeValues={':standard': 1}
         )['Items'][0]
         
     return record
@@ -95,7 +96,7 @@ def get_details(email, application_uuid):
     return resp
 
 
-def get_db_value(email, key_to_update, application_uuid):
+def get_db_value(email, key_to_update, application_uuid):    
     return get_details(email, application_uuid)['Item'].get(key_to_update, None)
 
 
@@ -136,20 +137,13 @@ def delete_document_info_from_database(user_email, event_body, application_uuid)
     return resp
 
 
-def handle_checkout_session_succeeded(checkout_session):
-    application_uuid = checkout_session.client_reference_id
+def handle_successful_payment(checkout_session):    
     email = checkout_session.customer_email
-    payment_intent = stripe.PaymentIntent.retrieve(
-        checkout_session.payment_intent
-    )
-    resp = payment_details_table.update_item(
-        Key={'email': email, 'application_uuid': application_uuid},
-        ExpressionAttributeNames={ "#the_key": 'details' },
-        ExpressionAttributeValues={ ":val_to_update": payment_intent },
-        UpdateExpression="SET #the_key = :val_to_update"
-    )
-
-    return resp
+    application_uuid = checkout_session.client_reference_id
+    
+    save_payment_info(email, application_uuid, checkout_session)
+    send_completed_application_email(email, application_uuid)
+    update_application_status(email, application_uuid)
 
 
 def get_file_size(b64string):
@@ -184,22 +178,62 @@ def send_email(subject, to_emails, body, attachment_string=None):
     return resp
 
 
-def build_csv(data):
-    resp = ''
-    for section, val in data.items():
-        if resp:
-            resp += '\n' * 3
+# def build_csv(data):
+#     resp = ''
+#     for section, val in data.items():
+#         if resp:
+#             resp += '\n' * 3
 
-        resp += section + '\n'
+#         resp += section + '\n'
 
-        for question, sval in val.items():
-            if question != 'undefined':
-                val = sval.get('val') or ''
-                resp += f'"{question}","{val}"\n'
+#         for question, sval in val.items():
+#             if question != 'undefined':
+#                 val = sval.get('val') or ''
+#                 resp += f'"{question}","{val}"\n'
 
-            if sval['sublist']:
-                for row in sval['sublist']:
-                    _row = [""] + [f'"{ii}"' for ii in row]
-                    resp += f'{",".join(_row)}\n'
+#             if sval['sublist']:
+#                 for row in sval['sublist']:
+#                     _row = [""] + [f'"{ii}"' for ii in row]
+#                     resp += f'{",".join(_row)}\n'
 
-    return resp
+#     return resp
+
+def send_completed_application_email(user_email, application_uuid):
+    try: 
+        subject = 'Turbocaid Application Summary'
+        to_emails = os.environ.get('TO_EMAILS', 'jason.5001001@gmail.com')
+        applicant_first_name = get_db_value(user_email, 'applicant_info_first_name', application_uuid)['value']
+        applicant_last_name = get_db_value(user_email, 'applicant_info_last_name', application_uuid)['value']
+        email_body = f'{applicant_first_name} {applicant_last_name} submitted application. Email: {user_email}. Application Id: {application_uuid}'
+
+        resp = send_email(subject, to_emails, email_body)
+    except Exception as err:
+        print('Error sending email with completed application info ' + str(err))
+
+
+def update_application_status(user_email, application_uuid):
+    try:
+        key_to_update = 'submitted_date'
+        now = datetime.datetime.now().isoformat()
+
+        resp = update_dynamodb(user_email, application_uuid, key_to_update, now)
+
+        return resp        
+    except Exception as err:
+        print('Error updating application status ' + str(err))
+
+
+def save_payment_info(user_email, application_uuid, checkout_session):
+    try:
+        payment_intent = stripe.PaymentIntent.retrieve(
+            checkout_session.payment_intent
+        )
+        resp = payment_details_table.update_item(
+            Key={'email': user_email, 'application_uuid': application_uuid},
+            ExpressionAttributeNames={ "#the_key": 'details' },
+            ExpressionAttributeValues={ ":val_to_update": payment_intent },
+            UpdateExpression="SET #the_key = :val_to_update"
+        )    
+    except Exception as err:
+        print('Error retrieving and saving stripe payment info ' + str(err))
+
