fd2f14229248d9c62b917473d9cdcc6893007d1f
Binyomin Cohen <binyominc@ltccs.com>
2020-09-14 16:41:25 -0400

    attempting non local endpoint

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,11 +5,10 @@ docker run -d -p 8000:8000 amazon/dynamodb-local
 #docker run -d --network host --name dyanolocal amazon/dynamodb-local
 sleep 5
 aws dynamodb create-table \
-    --table-name medicaid-details \
+    --table-name medicaid-details-code-build-test \
     --attribute-definitions \
         AttributeName=email,AttributeType=S \
         AttributeName=application_uuid,AttributeType=S \
     --key-schema AttributeName=email,KeyType=HASH AttributeName=application_uuid,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
-    --endpoint-url http://localhost:8000 \
     --region us-east-1
