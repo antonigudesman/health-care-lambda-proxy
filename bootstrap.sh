81723043f466f8097cbde17827d4aa7dcb0e70bb
Binyomin Cohen <binyominc@ltccs.com>
2020-09-14 15:21:48 -0400

    buildspec

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker pull amazon/dynamodb-local
-docker run -d -p --network host 8000:8000 amazon/dynamodb-local
+docker run -d -p 8000:8000 amazon/dynamodb-local
 #docker run -d --network host --name dyanolocal amazon/dynamodb-local
 sleep 5
 aws dynamodb create-table \
